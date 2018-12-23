aa1e5b859143bea6334ce45c30628132c09a9f9f
becs <becs@storypark.com>
2018-12-24 07:35:51 +1300

    Updates to controller and component for gif

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,12 @@ require 'HTTParty'
 attr_accessor :gif
 
   def index
+    @gif = new_url
+  end
+
+  def new_url
     list = HTTParty.get("https://api.giphy.com/v1/gifs/trending?api_key=dc1xlJCGMtxEQTEaRBIqFWnpbQW2MlOk&limit=25&rating=G")["data"]
     rando = rand(list.count)
-    @gif = list[rando]["images"]["original"]["url"]
+    list[rando]["images"]["original"]["url"]
   end
 end
--- app/javascript/components/GreetUser.js
+++ app/javascript/components/GreetUser.js
@@ -2,13 +2,12 @@ import React from "react"
 
 class GreetUser extends React.Component {
   render () {
-    console.log(this.props.giphy)
     return (
       <div>
         <h1 className="has-text-centered is-size-1 has-text-link"> Hello {this.props.name} </h1>
         <h2 className="is-pulled-left is-size-3 has-text-warning"> Enjoy a free Gif: </h2>
         <img src={this.props.giphy} />
-        <div className="button is-outlined is-link is-fullwidth is-large" onClick={console.log("click")}>Get another one!</div>
+        <div className="button is-outlined is-link is-fullwidth is-large" onClick={home.new_url}>Get another one!</div>
       </div>
     );
   }
